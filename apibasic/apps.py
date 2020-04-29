d9d5d559f055ff58976bb2cb92565e0eecbb2abe
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-04-30 01:12:35 +0530

    model serializer

---
+++ apibasic/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApibasicConfig(AppConfig):
+    name = 'apibasic'
---
+++ apibasic/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Article 
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article 
+        fields = ['id','title','author'] 
+ 
+
+"""
+class ArticleSerializer(serializers.Serializer):
+    title=serializers.CharField(max_length=100)
+    author=serializers.CharField(max_length=100)
+    email=serializers.EmailField(max_length=100)
+    date=serializers.DateTimeField()  
+
+
+    def create(self, validated_data):
+        return Article.objects.create(validated_data)
+
+
+    def update(self,instance,validated_data):
+        instance.title=validated_data.get('title',instance.title)
+        instance.author=validated_data.get('author',instance.author) 
+        instance.email=validated_data.get('email',instance.email)
+        instance.date=validated_data.get('date',instance.date)
+        instance.save() 
+        return instance
+"""
+
