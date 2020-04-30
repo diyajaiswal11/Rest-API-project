96303598cec48ce3cba5ac454a98f09ad497d5b1
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-05-01 03:30:16 +0530

    put and delete method

--- apibasic/urls.py
+++ apibasic/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('article/',views.article_list,name='article_list'),
+    path('detail/<int:pk>/',views.article_detail,name='article_detail'),
 ]
  
--- apibasic/views.py
+++ apibasic/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializer
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
-
+@csrf_exempt
 def article_list(request):
 
     if request.method=='GET':
@@ -23,3 +24,34 @@ def article_list(request):
             return JsonResponse(serializer.data,status=201) 
         return JsonResponse(serializer.errors,status=400)
 
+
+@csrf_exempt
+def article_detail(request,pk):
+    try:
+        article=Article.objects.get(pk=pk) 
+
+    except Article.DoesNotExist:
+        return HttpResponse(status=404) 
+
+    if request.method=='GET':
+        serializer=ArticleSerializer(article) 
+        return JsonResponse(serializer.data)  
+
+    elif request.method=='PUT':
+        data=JSONParser().parse(request)
+        serializer=ArticleSerializer(article,data=data)
+        if serializer.is_valid():
+            serializer.save() 
+            return JsonResponse(serializer.data) 
+        return JsonResponse(serializer.errors,status=400)  
+
+
+    elif request.method=='DELETE':
+        article.delete()
+        return HttpResponse(status=204)
+
+
+    
+
+
+    
