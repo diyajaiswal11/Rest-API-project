047922433ac4226b126f73d0a17cf9c52576c965
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-05-02 04:08:24 +0530

    api_view decorator

--- apibasic/serializers.py
+++ apibasic/serializers.py
@@ -4,7 +4,8 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article 
-        fields = ['id','title','author'] 
+        #fields = ['id','title','author','email']
+        fields='__all__' 
  
 
 """
--- apibasic/views.py
+++ apibasic/views.py
@@ -32,30 +32,36 @@ def article_list(request):
         #return JsonResponse(serializer.errors,status=400)
 
 
-@csrf_exempt
+@api_view(['GET','PUT','DELETE'])
 def article_detail(request,pk):
     try:
         article=Article.objects.get(pk=pk) 
 
     except Article.DoesNotExist:
-        return HttpResponse(status=404) 
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        #return HttpResponse(status=404) 
 
     if request.method=='GET':
         serializer=ArticleSerializer(article) 
-        return JsonResponse(serializer.data)  
+        return Response(serializer.data)
+        #return JsonResponse(serializer.data)  
 
     elif request.method=='PUT':
-        data=JSONParser().parse(request)
-        serializer=ArticleSerializer(article,data=data)
+        #data=JSONParser().parse(request)
+        serializer=ArticleSerializer(article,data=request.data)
+        #serializer=ArticleSerializer(article,data=data)
         if serializer.is_valid():
             serializer.save() 
-            return JsonResponse(serializer.data) 
-        return JsonResponse(serializer.errors,status=400)  
+            return Response(serializer.data)
+            #return JsonResponse(serializer.data) 
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        #return JsonResponse(serializer.errors,status=400)  
 
 
     elif request.method=='DELETE':
         article.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        #return HttpResponse(status=204)
 
 
     
